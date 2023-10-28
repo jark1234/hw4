339d5c0ae7fd68c56c589261f5a0dd8ceb7403ef
Jaclyn Turner <jaclyn.turner@sjsu.edu>
2023-10-27 21:05:57 -0700

    q4

--- bug1.py
+++ bug1.py
@@ -24,3 +24,35 @@ class Circle(Base):
      ,                  , '
        ' - , _ _ _ ,  '
                """
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+class Circle(Base):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})
+{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
+def main():
+	c = Circle(1,2,3)
+	print(c.shape())
+	print(c.draw())
+main()
+
