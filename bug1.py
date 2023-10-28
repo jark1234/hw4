134d03e3f3cfc07c7f4339019462a11f86c21308
Jaclyn Turner <jaclyn.turner@sjsu.edu>
2023-10-27 21:09:07 -0700

    q4

--- bug1.py
+++ bug1.py
@@ -3,37 +3,9 @@ class Base:
         self.x = x
         self.y = y
         self.size = size
-
 class Circle(Base):
     def __init__(self, x, y, size):
         super().__init__(x, y, size)
-
-    def draw(self):
-        return f"""
-({self.x}, {self.y})
-{self.size}
-         , - ~ ~ ~ - ,
-     , '               ' ,
-   ,                       ,
-  ,                         ,
- ,                           ,
- ,                           ,
- ,                           ,
-  ,                         ,
-   ,                       ,
-     ,                  , '
-       ' - , _ _ _ ,  '
-               """
-class Base:
-    def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
-        self.size = size
-
-class Circle(Base):
-    def __init__(self, x, y, size):
-        super().__init__(x, y, size)
-
     def draw(self):
         return f"""
 ({self.x}, {self.y})
