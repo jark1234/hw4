58997642b2e6ba6a24a5c5649d0a4d3c9965f0e2
Jaclyn Turner <jaclyn.turner@sjsu.edu>
2023-10-27 19:42:25 -0700

    q1

--- ladder.py
+++ ladder.py
@@ -1,11 +1,8 @@
 def my_steps(n):
 	if not 1 <= n <= 25:
-		raise ValueError("Input out of bounds. Please enter a 
-number between 1 and 25.")
+		raise ValueError("Input out of bounds.")
 	climbing_steps = [0] * (n+1)
 	climbing_steps[0] = 1
 	for i in range(1, n+1):
-		climbing_steps[i] = climbing_steps[i-1] + 
-climbing_steps[i-2]
+		climbing_steps[i] = climbing_steps[i-1] + climbing_steps[i-2]
 	return climbing_steps[n]
-
