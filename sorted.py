c250e928260fe4c2cb1ca3c252a53ea3930d823f
Jaclyn Turner <jaclyn.turner@sjsu.edu>
2023-10-27 20:10:49 -0700

    q3

--- sorted.py
+++ sorted.py
@@ -1,7 +1,5 @@
 def reverse_sort_dictionary(directory)
 	if not isinstance(directory, dict):
 		raise TypeError("Input should be a dictionary.")
-	sorted_list = [(name, details[0]) for name, details in 
-sorted(directory.items(), key=lambda x: x[0], reverse=True)]
+	sorted_list = [(name, details[0]) for name, details in sorted(directory.items(), key=lambda x: x[0], reverse=True)]
 	return sorted_list
-
