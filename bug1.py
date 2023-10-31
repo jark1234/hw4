e46f69398a9b866c6ea397096c7690af2ff8a29c
Jaclyn Turner <jaclyn.turner@sjsu.edu>
2023-10-31 16:37:22 -0700

    idk at this point

--- bug1.py
+++ bug1.py
@@ -1,9 +1,3 @@
-class Base:
-    def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
-        self.size = size
-
 class Circle(Base):
     def __init__(self, x, y, size):
         super().__init__(x, y, size)
@@ -25,17 +19,6 @@ class Circle(Base):
        ' - , _ _ _ ,  '
                """
 
-def draw_circle(mycircle):
-    print(mycircle.draw())
-
-def shape(myshape):
-    print(myshape.draw())
-
-def main():
-    c = Circle(1, 2, 3)
-    shape(c)
-    draw_circle(c)
-
-if __name__ == "__main__":
-    main()
+    def shape(self):
+        return f"({self.x}, {self.y})\n{self.size}"
 
